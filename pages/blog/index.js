892f1b5cd16dc56ce3005f749880ac621095551c
Tej <tejpartapsingh91@gmail.com>
2023-05-22 16:50:36 -0400

    s

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -25,6 +25,10 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>Blog</h1>
+        <p>
+          Here on this page, you will find a list of all the articles I have
+          written and published.
+        </p>
         <hr />
         <h2>Archive</h2>
         <div className={styles.articles}>{articles}</div>
