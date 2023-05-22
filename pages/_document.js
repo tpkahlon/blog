087a14fd3b5ce979e7bbdbf61029bd884dcb4f73
Tej <tejpartapsingh91@gmail.com>
2023-05-22 16:40:37 -0400

    f

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script
-          src="https://cdn.counter.dev/script.js"
-          data-id="609bef29-f039-4bea-b906-2130067d225f"
-          data-utcoffset="-5"
-        />
         <meta
           name="description"
           content="Crafting captivating digital experiences with the power of JavaScript, React, CSS and a passion for front-end excellence."
