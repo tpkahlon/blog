70cfc324111ec519537f725809e03cc71475978b
Tej <tejpartapsingh91@gmail.com>
2023-05-26 20:12:42 -0400

    f

--- contentful/index.js
+++ contentful/index.js
@@ -5,14 +5,14 @@ const { ENVIRONMENT } = process.env;
 const devOptions = {
   environment: "master",
   host: "preview.contentful.com",
-  space: process.env.SPACE,
-  accessToken: process.env.CPATOKEN,
+  space: "rv8g4s0yjblf",
+  accessToken: "z1ndlIRQT8C5eynhI1BsKRf44hqINVkj0MkRSW6cGC0",
 };
 
 const productionOptions = {
   environment: "master",
-  space: process.env.SPACE,
-  accessToken: process.env.CDATOKEN,
+  space: "rv8g4s0yjblf",
+  accessToken: "9LGAzdfrZ9Di11IXb65jePOEw1J9I4ZQcrZBiR2Ugjw",
 };
 
 const options = ENVIRONMENT === "local" ? devOptions : productionOptions;
