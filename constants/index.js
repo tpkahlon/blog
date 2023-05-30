14c871d51602ab90f6664740eb1c63d45c661f61
Tej <tejpartapsingh91@gmail.com>
2023-05-29 23:29:20 -0400

    f1

--- constants/index.js
+++ constants/index.js
@@ -1,21 +1,23 @@
-export const isLocal = process.env.ENVIRONMENT === "local";
+const isLocal = process.env.ENVIRONMENT === "local";
 
-export const devOptions = {
+const devOptions = {
   environment: "master",
   host: "preview.contentful.com",
   space: "rv8g4s0yjblf",
   accessToken: "z1ndlIRQT8C5eynhI1BsKRf44hqINVkj0MkRSW6cGC0",
 };
 
-export const productionOptions = {
+const productionOptions = {
   environment: "master",
   space: "rv8g4s0yjblf",
   accessToken: "9LGAzdfrZ9Di11IXb65jePOEw1J9I4ZQcrZBiR2Ugjw",
 };
 
-export const contentfulClient = isLocal ? devOptions : productionOptions;
+const contentfulClient = isLocal ? devOptions : productionOptions;
 
 export default {
   contentfulClient,
+  devOptions,
   isLocal,
+  productionOptions,
 };
